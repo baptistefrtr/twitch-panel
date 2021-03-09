ebb3632ad14787325a34473d97e0d3aa117e5108
baptistefrtr <baptistefrtr@gmail.com>
2021-03-09 19:30:17 +0100

    Init + contact form

---
+++ workshop/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <b-row>
+      <b-col>
+        <b-navbar toggleable="sm" type="light" variant="light">
+          <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-navbar-brand><a href="/">
+          Twitch Panel</a></b-navbar-brand>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <router-link to="/" style="padding-right: 10px">Home</router-link>
+              <router-link to="/about" style="padding-right: 10px">Settings</router-link>
+              <router-link to="/contact">Contact</router-link>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-navbar>
+      </b-col>
+    </b-row>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ workshop/src/views/Contact.vue
@@ -0,0 +1,65 @@
+<template>
+  <b-row style="margin-top: 10%; text-align: center">
+    <b-col cols="3"></b-col>
+    <b-col cols="6">
+      <b-card>
+        <b-card-title> Contact form </b-card-title>
+        <b-card-text style="margin-top: 10%">
+          <p>If you have any issue contact us here.</p>
+        </b-card-text>
+        <b-input-group prepend="Email" class="mt-3">
+          <b-form-input v-model="email"></b-form-input>
+        </b-input-group>
+        <b-input-group prepend="Username" class="mt-3">
+          <b-form-input v-model="username"></b-form-input>
+        </b-input-group>
+        <b-input-group prepend="Message" class="mt-3">
+          <b-form-input v-model="message"></b-form-input>
+        </b-input-group>
+        <br />
+        <b-button @click="sendMail()" pill variant="outline-primary">Submit</b-button>
+      </b-card>
+    </b-col>
+    <b-col cols="3"></b-col>
+  </b-row>
+</template>
+
+<script>
+import emailjs from "emailjs-com";
+
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      message: "",
+    };
+  },
+  methods: {
+    sendMail() {
+      emailjs
+        .send(
+          "service_ranl2il",
+          "template_7mrerg7",
+          {
+            name: this.username,
+            email: this.email,
+            message: this.message,
+          },
+          "user_YI9458H3GSgaBYynbKmaC"
+        )
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          function (error) {
+            console.log("FAILED...", error);
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
